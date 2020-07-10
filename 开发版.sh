2764945f192b6e6efb1944b1fe8f3d02eec16f5a
Kemmu <43158315+jianwuL@users.noreply.github.com>
2020-07-10 18:13:53 +0800

    Update 开发版.sh

--- 开发版.sh
+++ 开发版.sh
@@ -6,10 +6,10 @@ sed -i 's/OpenWrt/$(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default
 
 #添加额外软件包
 git clone https://github.com/lisaac/luci-app-dockerman.git package/openwrt-packages/luci-app-dockerman
-svn co https://github.com/bin20088/openwrt-package/trunk/package/brook package/openwrt-packages/brook
-svn co https://github.com/bin20088/openwrt-package/trunk/package/chinadns-ng package/openwrt-packages/chinadns-ng
-svn co https://github.com/bin20088/openwrt-package/trunk/package/tcping package/openwrt-packages/tcping
-svn co https://github.com/bin20088/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
+svn co https://github.com/Lienol/openwrt-package/trunk/package/brook package/openwrt-packages/brook
+svn co https://github.com/Lienol/openwrt-package/trunk/package/chinadns-ng package/openwrt-packages/chinadns-ng
+svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping package/openwrt-packages/tcping
+svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
 git clone https://github.com/tty228/luci-app-serverchan.git package/openwrt-packages/luci-app-serverchan
 git clone https://github.com/bin20088/luci-theme-argon-mc.git package/openwrt-packages/luci-theme-argon-mc
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/openwrt-packages/luci-theme-opentomcat
@@ -18,7 +18,7 @@ git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/o
 git clone https://github.com/Repobor/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
 git clone https://github.com/bin20088/luci-app-koolddns.git package/openwrt-packages/luci-app-koolddns
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/openwrt-packages/luci-app-ssr-plus
-svn co https://github.com/bin20088/OpenClash/trunk/luci-app-openclash package/openwrt-packages/luci-app-openclash
+svn co https://github.com/0saga0/OpenClash/trunk/luci-app-openclash package/openwrt-packages/luci-app-openclash
 svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom package/openwrt-packages/luci-theme-infinityfreedom
 #赋予koolddns权限
 chmod 0755 package/openwrt-packages/luci-app-koolddns/root/etc/init.d/koolddns
